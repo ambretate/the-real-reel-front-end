6ab5ddf12b1d81aa0fd0fb50173fbfa376975627
Jeremy Wiles-Young <jwilesyoung@gmail.com>
2024-03-19 10:31:24 -0400

    format header start css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Header from './Components/Header/Header.jsx';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     Hello World
+     
     </div>
   );
 }
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import logo from '../../assets/images/TRR-LOGO-1.png';
+import './Header.css';
 
 function Header() {
   return (
-    <div>Header</div>
+    <div id="background-Header">
+        
+        <div id="logoContainer-Header">
+
+            <img id="logo-Header" src={logo} alt="the real reel logo, an old style film camera pointing to the right, stylized in solid black and white" />
+            
+        </div>
+
+        <div id="container-Header">
+            <button id="signInButton-Header">Login/Logout</button>
+
+        </div>
+    </div>
   )
 }
 
