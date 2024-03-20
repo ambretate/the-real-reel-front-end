65b2a4dab744137c079792fa6622c526c03fdbed
Jeremy Wiles-Young <jwilesyoung@gmail.com>
2024-03-20 19:25:28 -0400

    hook up catalog routes in App.js and NavLinks in Catalog take us to Movie route with respective id in path

--- src/Screens/Movie/Movie.jsx
+++ src/Screens/Movie/Movie.jsx
@@ -1,53 +1,52 @@
-import {useEffect, useState, useParams} from 'react';
+import {useEffect, useState} from 'react';
+import { useParams } from 'react-router-dom';
 import MovieBlock from '../../Components/MovieBlock/MovieBlock.jsx';
 import PreReview from '../../Components/PreReview/PreReview.jsx';
 import MoviePreview from '../../Components/MoviePreview/MoviePreview.jsx';
 import { getReviews } from '../../Services/reviews.js';
+import { getMovie } from '../../Services/movies.js';
+import './Movie.css';
 
 function Movie() {
   // get id with useParams
-  const {movieId} = useParams();
+  const { id } = useParams();
+  console.log('print params', id);
 
   // set states
+  const [movie, setMovie ] = useState({});
   const [reviews, setReviews] = useState([]);
   const [recs, setRecs] = useState([]);
   
-  // make function to fetch movie
+  // fetch the movie with ID
   useEffect(() => {
-    const fetchReviews = async () => {
-      const allReviews = await getReviews();
-      setReviews(allReviews);
+    const fetchMovie = async () => {
+      const item = await getMovie(id);
+      setMovie(item.movie);
+      setReviews(item.reviews);
     };
     
-    fetchReviews();
-  }, [])
+    fetchMovie();
+  }, [id]);
 
-  // make function to populate reviews
-  useEffect(() => {
-    const fetchReviews = async () => {
-      const allReviews = await getReviews();
-      setReviews(allReviews);
-    };
-    
-    fetchReviews();
-  }, [])
+  //its movie.movie :)
+  console.log('title reviews', movie, reviews);
 
-  // make function to populate recommendations
-  useEffect( () => {
+
+  // make function to populate recommendations, post MVP
+  // useEffect( () => {
     
-  }, []);
+  // }, []);
 
   return (
     <div id="mainContain-Movie">
-      <MovieBlock id={movieId}/>
-      <div id="reviews-Movie">
-        <PreReview id={reviews[0]} />
-      </div>
-      <div id="recommendations-Movie">
-        <MoviePreview id={reviews[0]}/>
-      </div>
-
-
+      <h1 id="title-Movie">{movie.title}</h1>  
+      <img 
+        id="image-Movie"
+        src={movie.image} 
+        alt={`${movie.title}'s main poster`} 
+      />
+      <p id="description-Movie">{movie.description}</p>
+      
     </div>
   )
 }
