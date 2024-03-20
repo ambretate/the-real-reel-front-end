f8e5c9b37bccd667ae9f67a0d71adcc2db01d689
Jeremy Wiles-Young <jwilesyoung@gmail.com>
2024-03-20 16:13:51 -0400

    some small changes to Movie

--- src/Screens/Movie/Movie.jsx
+++ src/Screens/Movie/Movie.jsx
@@ -1,17 +1,36 @@
-import {useEffect, useState} from 'react';
+import {useEffect, useState, useParams} from 'react';
 import MovieBlock from '../../Components/MovieBlock/MovieBlock.jsx';
 import PreReview from '../../Components/PreReview/PreReview.jsx';
 import MoviePreview from '../../Components/MoviePreview/MoviePreview.jsx';
+import { getReviews } from '../../Services/reviews.js';
 
-function Movie({ id }) {
+function Movie() {
+  // get id with useParams
+  const {movieId} = useParams();
+
   // set states
   const [reviews, setReviews] = useState([]);
   const [recs, setRecs] = useState([]);
   
-  // make function to populate reviews
-  useEffect( () => {
+  // make function to fetch movie
+  useEffect(() => {
+    const fetchReviews = async () => {
+      const allReviews = await getReviews();
+      setReviews(allReviews);
+    };
     
-  }, []);
+    fetchReviews();
+  }, [])
+
+  // make function to populate reviews
+  useEffect(() => {
+    const fetchReviews = async () => {
+      const allReviews = await getReviews();
+      setReviews(allReviews);
+    };
+    
+    fetchReviews();
+  }, [])
 
   // make function to populate recommendations
   useEffect( () => {
@@ -20,7 +39,7 @@ function Movie({ id }) {
 
   return (
     <div id="mainContain-Movie">
-      <MovieBlock />
+      <MovieBlock id={movieId}/>
       <div id="reviews-Movie">
         <PreReview id={reviews[0]} />
       </div>
