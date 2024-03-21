77fe51d5778c50589bb1d73486eb9287371c1933
Jeremy Wiles-Young <jwilesyoung@gmail.com>
2024-03-21 15:16:22 -0400

    fix conversion function flow in Movie and MovieBlock

--- src/Screens/Movie/Movie.jsx
+++ src/Screens/Movie/Movie.jsx
@@ -2,11 +2,9 @@ import {useEffect, useState} from 'react';
 import { useParams } from 'react-router-dom';
 import MovieBlock from '../../Components/MovieBlock/MovieBlock.jsx';
 import PreReview from '../../Components/PreReview/PreReview.jsx';
-import MoviePreview from '../../Components/MoviePreview/MoviePreview.jsx';
 import Header from '../../Components/Header/Header.jsx';
 import Footer from '../../Components/Footer/Footer.jsx';
 import { getMovie } from '../../Services/movies.js';
-import { parseDate } from '../../Services/conversions.js';
 import './Movie.css';
 
 function Movie() {
@@ -40,18 +38,7 @@ function Movie() {
   return (
     <div id="mainContain-Movie">
       <Header/>
-        <h1 id="title-Movie">{movie.title}</h1>  
-        <img 
-          id="image-Movie"
-          src={movie.image} 
-          alt={`${movie.title}'s main poster`} 
-        />
-        <p id="dataBullets-Movie">{(movie.releaseDate) ? 
-              `Release Date: ${parseDate( movie.releaseDate )}` 
-              : null }
-        </p>
-        <p id="description-Movie">{movie.description}</p>
-
+        <MovieBlock movie={movie}/>
         {
           reviews.map( (item, idx) => (
             <PreReview movie={item} review={reviews[idx]} key={idx} />
