e51f4a1d9b2101a6c3937053c65454da1a640a94
nilestoomer <user@email.com>
2024-03-22 13:46:38 -0400

    footer done

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,8 +1,16 @@
 import './Footer.css';
+import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
     <div id='background-Footer'>
+      <Link to={"https://github.com/ambretate/the-real-reel-front-end"}>
+        <img
+          className='github-footer'
+          src='https://logos-world.net/wp-content/uploads/2020/11/GitHub-Symbol.png'
+          alt='github'
+        />
+      </Link>
       <p id='brand-Footer'>Real Reel 2024</p>
     </div>
   )
