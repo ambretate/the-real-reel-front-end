9bb697d25af5486e519f2157070a7296915fed91
zackcinal <zekeriyacnl@gmail.com>
2024-03-22 16:12:22 -0400

    update everything

--- src/Components/PreReview/PreReview.jsx
+++ src/Components/PreReview/PreReview.jsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from 'react';
-import { getUser } from '../../Services/users.js';
-import './PreReview.css';
+import { useEffect, useState } from "react";
+import { getUser } from "../../Services/users.js";
+import "./PreReview.css";
 
-function PreReview( { movie, review, showUser } ) {
+function PreReview({ movie, review, showUser }) {
   const [user, setUser] = useState({});
 
   // fetch the userData with ID
@@ -10,59 +10,44 @@ function PreReview( { movie, review, showUser } ) {
     const fetchUser = async () => {
       const item = await getUser(review.userID);
       setUser(item);
-      
     };
-    
+
     fetchUser();
   }, [review]);
-  
-  const userName = (!user) ? 'loading ...' : user.username;
+
+  const userName = !user ? "loading ..." : user.username;
   return (
     <div id="mainContainer-PreReview">
       <div id="leftContainer-PreReview">
-        {
-          (!showUser) ? 
-            <img 
-              id="movieImage-PreReview"
-              src={movie.image}
-              alt={`Movie Title poster for ${movie.title}`}
-            />
-           
-          : 
-            <h1 id="userName-PreReview">
-              {userName}
-            </h1>
-        } 
-
+        {!showUser ? (
+          <img
+            id="movieImage-PreReview"
+            src={movie.image}
+            alt={`Movie Title poster for ${movie.title}`}
+          />
+        ) : (
+          <h1 id="userName-PreReview">{userName}</h1>
+        )}
       </div>
 
       <div id="rightContainer-PreReview">
-        {
-          (!showUser) ? 
-            <h1 id="movieTitle-PreReview">
-              {movie.title}
-            </h1>
-
-          : null
-            
-        } 
+        <div className="spoilerTitle-container">
+          <h1 id="movieTitle-PreReview">{movie.title}</h1>
+          {review.hasSpoilers ? <button>Spoilers!!!</button> : null}
+        </div>
 
         <h2 id="reviewTitle-PreReview">
-          <u>{review.title}</u> By {userName} 
+          <u>{review.title}</u> By {userName}
         </h2>
-        
-        {
-          (review.hasSpoilers)
-          ? <p id="body-PreReview"> CONTAINS SPOILERS! </p>
-          
-          : <p id="body-PreReview">
-              {review.review}
-            </p>
-        }
+
+        {/* {review.hasSpoilers ? (
+          <p id="body-PreReview"> CONTAINS SPOILERS! </p>
+        ) : ( */}
+          <p id="body-PreReview">{review.review}</p>
+        {/* )} */}
       </div>
-      
     </div>
-  )
+  );
 }
 
-export default PreReview
+export default PreReview;
