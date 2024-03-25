e37d9433abb68b7a4f879e37272d3df5021490fd
Jeremy Wiles-Young <jwilesyoung@gmail.com>
2024-03-25 10:07:56 -0400

    we merged

--- src/Screens/Movie/Movie.jsx
+++ src/Screens/Movie/Movie.jsx
@@ -24,12 +24,7 @@ function Movie({ user }) {
       const item = await getMovie(id);
       const followsData = await getFollows();
       setMovie(item.movie);
-      setReviews(item.reviews);
-<<<<<<< HEAD
-      //console.log('checkout what a review obj loook like', item.reviews);
-=======
->>>>>>> b199dd24d31c99b4b80542e2ada1ce396d7eb26f
-      setFollows(followsData);
+      setReviews(item.reviews);setFollows(followsData);
     };
 
     fetchMovie();
@@ -44,45 +39,7 @@ function Movie({ user }) {
         <div id="backCatalog-PreReview">
           <button>Back to Catalog</button>
         </div>
-<<<<<<< HEAD
-        <NavLink to="/catalog">
-          <div id='backCatalog-PreReview'>
-            <p style={{"textAlign": "center"}}>back to Catalog</p>
-          </div>
-        </NavLink>
-        <div className="review-container">
-          <h3 className="write-a-review">Write a review for {movie.title}</h3>
-          <CreateReview
-            setToggleReviews={setToggleReviews}
-            movieID={movie._id}
-            userID={user?.id}
-          />
-          <h1 id="h1-Movie">Reviews</h1>
-          <div id="reviewContainer-Movie">
-            {
-              (reviews) ?
-                reviews.map((review, idx) => {
-                  //console.log('this is the review.map output',review, idx);
-                  let isFollowingUser = follows.following.some(
-                    (follow) => follow._id === review.userID
-                  );
-                  return (
-                    <PreReview
-                      movie={movie}
-                      review={review}
-                      key={idx}
-                      index={idx}
-                      showUser={false}
-                      userID={user?.id}
-                      isFollowingUser={isFollowingUser}
-                      setToggleReviews={setToggleReviews}
-                    />
-                  );
-                })
-              : null
-            }
-          </div>
-=======
+
       </NavLink>
       <div className="review-container">
         <h3 className="write-a-review">Write a review for {movie.title}</h3>
@@ -112,7 +69,6 @@ function Movie({ user }) {
                 );
               })
             : null}
->>>>>>> b199dd24d31c99b4b80542e2ada1ce396d7eb26f
         </div>
       </div>
     </div>
